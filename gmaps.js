6d9f8c845917e06b2180932c50da9a9abac1abf8
Joanne Zanetton <joannezanetton@gmail.com>
2015-06-30 13:22:11 +0100

    Shows map

--- gmaps.js
+++ gmaps.js
@@ -528,6 +528,10 @@ GMaps.prototype.createControl = function(options) {
   if (options.id) {
     control.id = options.id;
   }
+  
+  if (options.title) {
+    control.title = options.title;
+  }
 
   if (options.classes) {
     control.className = options.classes;
